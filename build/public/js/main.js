b3f9ea7172c80b70c76eff2f4bccb098a8d43aa7
Max <maxmonax@gmail.com>
2022-06-14 22:11:34 +0300

    plashka 1 integrated into index.html

--- build/public/js/main.js
+++ build/public/js/main.js
@@ -1,8 +1,13 @@
 function showAvatar() {
-  document.querySelector('#showedAvatar').style.left = '0vw'; document.querySelector('#showedAvatar').classList.add('showedAvatar'); document.querySelector('#showedAvatar').classList.remove('hiddenAvatar');
-  setTimeout(() => {document.querySelector('#showedAvatar').style.opacity = '1'}, 300)
+    document.querySelector('#showedAvatar').style.left = `${Math.trunc(Math.random() * 1000)}px`;
+    document.querySelector('#showedAvatar').style.top = `${Math.trunc(Math.random() * 400)}px`;
+    document.querySelector('#showedAvatar').classList.add('showedAvatar');
+    document.querySelector('#showedAvatar').classList.remove('hiddenAvatar');
+    setTimeout(() => { document.querySelector('#showedAvatar').style.opacity = '1' }, 300)
 }
 
 function hiddenAvatar() {
-  document.querySelector('#showedAvatar').classList.remove('showedAvatar');document.querySelector('#showedAvatar').classList.add('hiddenAvatar'); setTimeout(() => {document.querySelector('#showedAvatar').style.opacity = '0'}, 500);
+    document.querySelector('#showedAvatar').classList.remove('showedAvatar');
+    document.querySelector('#showedAvatar').classList.add('hiddenAvatar');
+    setTimeout(() => { document.querySelector('#showedAvatar').style.opacity = '0' }, 500);
 }
