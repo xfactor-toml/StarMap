d8b9c52e86a9e4ab1419248bb2c97608906d4d88
Max <maxmonax@gmail.com>
2023-06-15 21:06:37 +0800

    Update StarMath.ts

--- src/math/StarMath.ts
+++ src/math/StarMath.ts
@@ -5,33 +5,38 @@ export class StarMath {
     
     public static getMassValues(aStarParams: ServerStarParams): {
         value: number,
-        percent: number
+        percent: number,
+        unit: string
     } {
         const min = 10000;
         const max = 10000000;
         let res = {
             value: aStarParams.mass,
-            percent: MyMath.clamp(aStarParams.mass / max, 0, 1)
+            percent: MyMath.clamp(aStarParams.mass / max, 0, 1),
+            unit: 'E.M.'
         }
         return res;
     }
 
     public static getSlotsValues(aStarParams: ServerStarParams): {
         value: number,
-        percent: number
+        percent: number,
+        unit: string
     } {
         const min = 1;
         const max = 100;
         let res = {
             value: aStarParams.planetSlots,
-            percent: MyMath.clamp(aStarParams.planetSlots / max, 0, 1)
+            percent: MyMath.clamp(aStarParams.planetSlots / max, 0, 1),
+            unit: ''
         }
         return res;
     }
 
     public static getTotalEnergyValues(aStarParams: ServerStarParams): {
         value: number,
-        percent: number
+        percent: number,
+        unit: string
     } {
         const min = 0.01;
         const max = 1000;
@@ -39,29 +44,33 @@ export class StarMath {
         const realValue = aStarParams.fuel / factor;
         let res = {
             value: realValue,
-            percent: MyMath.clamp(realValue / max, 0, 1)
+            percent: MyMath.clamp(realValue / max, 0, 1),
+            unit: ''
         }
         return res;
     }
 
     public static getEnergyPerHourValues(aStarParams: ServerStarParams): {
         value: number,
-        percent: number
+        percent: number,
+        unit: string
     } {
         const factorView = 1e9;
         const factor = 1e18;
         const min = 0.000002314 * factor;
         const max = 0.042651648 * factor;
         let res = {
-            value: aStarParams.fuelSpendings / factorView,
-            percent: MyMath.clamp(aStarParams.fuelSpendings / max, 0, 1)
+            value: Math.trunc(aStarParams.fuelSpendings / factorView),
+            percent: MyMath.clamp(aStarParams.fuelSpendings / max, 0, 1),
+            unit: 'GWei/hour'
         }
         return res;
     }
 
     public static getLifeValues(aStarParams: ServerStarParams): {
         value: number,
-        percent: number
+        percent: number,
+        unit: string
     } {
         const HoursByLevel = {
             1: 4380,
@@ -73,8 +82,10 @@ export class StarMath {
         const starMaxHours = HoursByLevel[aStarParams.level];
         const e = aStarParams.fuel / aStarParams.fuelSpendings;
         let res = {
-            value: e,
-            percent: MyMath.clamp(e / starMaxHours, 0, 1)
+            // value: e,
+            value: Math.trunc(MyMath.clamp(e / starMaxHours, 0, 1) * 100),
+            percent: MyMath.clamp(e / starMaxHours, 0, 1),
+            unit: '%'
         }
         return res;
     }
