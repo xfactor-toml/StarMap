b7d2e195d81b370b0ef53572b1ce56aa2feeffbd
Yuriy <berlandspw@gmail.com>
2023-07-18 10:36:30 +0300

    Game auth type update

--- src/blockchain/functions/gameplay.ts
+++ src/blockchain/functions/gameplay.ts
@@ -2,45 +2,54 @@ import { w3cwebsocket as WebSocket } from 'websocket';
 import Web3 from "web3";
 import { wsServerUrl, env } from "../config";
 
-export async function GameAuth ( account : string ) : Promise<string> {
+export async function GameAuth(account: string): Promise<WebSocket | null> {
+  if (!env || !account) {
+    return null;
+  }
 
-     if (!env || !account) {
-        return ""
-     }
-     
-     const wss = new WebSocket(wsServerUrl)
-     const web3 = new Web3(env)
+  const wss = new WebSocket(wsServerUrl);
+  const web3 = new Web3(env);
 
-     return await new Promise(resolve => {
-        wss.onopen = (ws : WebSocket) => {
-            wss.onmessage = async (message : any) => {
+  return await new Promise((resolve) => {
+    wss.onopen = (ws: WebSocket) => {
+      let isAuthRequested = false;
+      wss.onmessage = async (message: any) => {
+        try {
+          const msg = JSON.parse(message.data);
 
-                try {
-                    const msg = JSON.parse(message.data)
+          if (msg.action === "auth" && msg.state === "success") {
+            wss.onmessage = null;
+            resolve(wss);
+            return wss;
+          }
 
-                    if (msg.action === "auth" && msg.state === "success") {
-                        resolve(ws)
-                        return ws
-                    } 
-                    
-                    const dt = new Date().getTime()
-                    const signMsg = "auth_" + String(dt - (dt % 600000))
-                    const signature = await web3.eth.personal.sign(signMsg, account, '')
-                    wss.send(JSON.stringify({
-                        action: "auth",
-                        signature: signature
-                    }))
-                } catch (e) {
-                    console.log(e.message)
-                }
-            }
-
-            wss.onclose = () => {
-                resolve('')
-                return ''
-            }
+          if (!isAuthRequested) {
+            const dt = new Date().getTime();
+            const signMsg = "auth_" + String(dt - (dt % 600000));
+            const signature = await web3.eth.personal.sign(
+              signMsg,
+              account,
+              ""
+            );
+            wss.send(
+              JSON.stringify({
+                action: "auth",
+                signature: signature,
+              })
+            );
+            isAuthRequested = true;
+          }
+        } catch (e) {
+          console.log(e.message);
         }
-     })
+      };
+
+      wss.onclose = () => {
+        resolve(null);
+        return null;
+      };
+    };
+  });
 }
 
 export async function EnterGame (ws : WebSocket) {
