b29b9bd1e1ad585eeae65e30c53e1366b6abae54
Max <maxmonax@gmail.com>
2024-05-11 03:20:37 +0500

    v.402 up

--- src/game/data/GlobalParams.ts
+++ src/game/data/GlobalParams.ts
@@ -30,7 +30,7 @@ export type GalaxyData = {
  */
 export class GlobalParams {
 
-    static version = 'v0.3.401';
+    static version = 'v0.3.402';
     static isDebugMode = false;
     static domCanvasParent: HTMLElement = null;
     static domTouchParent: HTMLElement = null;
