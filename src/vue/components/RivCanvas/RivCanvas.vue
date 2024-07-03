f660db93462b32397ef7478712ccc013ff76de3f
Gorkunov Maksim <maks.ark@gmail.com>
2024-07-04 00:53:53 +0300

    add rive animation

---
+++ src/vue/components/RivCanvas/RivCanvas.vue
@@ -0,0 +1,30 @@
+<template>
+  <canvas
+    ref="canvas"
+    class="RivCanvas"
+    width="200"
+    height="200"
+  />
+</template>
+
+<script lang="ts">
+import { Rive } from "@rive-app/canvas";
+
+export default {
+  name: 'RivCanvas',
+  data() {
+    return {
+      riv: null
+    }
+  },
+  mounted() {
+    this.riv = new Rive({
+        src: '/rocket.riv',
+        canvas: this.$refs.canvas,
+        autoplay: true,
+    });
+  }
+};
+</script>
+
+<style scoped src="./RivCanvas.css"></style>
--- src/vue/scenes/StartScene/StartScene.vue
+++ src/vue/scenes/StartScene/StartScene.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="StartScene">
     <!-- <PowerIndicator/> -->
+    <RivCanvas/>
     <component :is="scenesStore.current.mode.getComponent()" />
   </div>
 </template>
 
 <script lang="ts">
 import { useScenesStore } from '@/stores';
-import { PowerIndicator } from '@/components';
+import { PowerIndicator, RivCanvas } from '@/components';
 import { mapStores } from 'pinia';
 
 export default {
   name: 'StartScene',
   components: {
-    PowerIndicator
+    PowerIndicator,
+    RivCanvas
   },
   computed: mapStores(useScenesStore),
 };
