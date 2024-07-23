de08ed622777ef5e1f8bbe3e1c641572776d98e7
Max <maxmonax@gmail.com>
2024-07-24 01:26:25 +0300

    refactoring

--- src/vue/App.vue
+++ src/vue/App.vue
@@ -1,10 +1,9 @@
 <template>
-  <transition :css="false" @enter="onEnter" @leave="onLeave" @after-leave="onAfterLeave">
-    <component :is="scenesStore.current.scene.getComponent()" />
-  </transition>
-  <div class="version">{{ version }}</div>
-  <div class="version 2">{{ version }}</div>
-  <WalletConnectPopup v-if="walletStore.popup" @close="walletStore.hidePopup" />
+    <transition :css="false" @enter="onEnter" @leave="onLeave" @after-leave="onAfterLeave">
+      <component :is="scenesStore.current.scene.getComponent()" />
+    </transition>
+    <div class="version">{{ version }}</div>
+    <WalletConnectPopup v-if="walletStore.popup" @close="walletStore.hidePopup" />
 </template>
 
 <script lang="ts">
@@ -19,69 +18,69 @@ import { WalletConnectPopup } from '@/components/WalletConnectPopup';
 import { GlobalParams } from '~/game/data/GlobalParams';
 
 export default {
-  name: 'App',
-  components: {
-    WalletConnectPopup
-  },
-  data: () => ({
-    version: GlobalParams.galaxyVersion
-  }),
-  computed: mapStores(
-    useScenesStore,
-    useSettingsStore,
-    useUiStore,
-    useWalletStore
-  ),
-  methods: {
-    async onEnter(el, done) {
-      await anime({
-        targets: el,
-        easing: 'easeInOutQuart',
-        duration: 400,
-        opacity: [0, 1],
-      }).finished
-
-      done()
+    name: 'App',
+    components: {
+      WalletConnectPopup
     },
-    async onLeave(el, done) {
-      await anime({
-        targets: el,
-        easing: 'easeInOutQuart',
-        duration: 400,
-        opacity: [1, 0],
-      }).finished
+    data: () => ({
+      version: GlobalParams.galaxyVersion
+    }),
+    computed: mapStores(
+      useScenesStore,
+      useSettingsStore,
+      useUiStore,
+      useWalletStore
+    ),
+    methods: {
+      async onEnter(el, done) {
+        await anime({
+          targets: el,
+          easing: 'easeInOutQuart',
+          duration: 400,
+          opacity: [0, 1],
+        }).finished
 
-      done()
+        done()
+      },
+      async onLeave(el, done) {
+        await anime({
+          targets: el,
+          easing: 'easeInOutQuart',
+          duration: 400,
+          opacity: [1, 0],
+        }).finished
+
+        done()
+      },
+      onAfterLeave() {
+        this.scenesStore.previous.scene?.afterLeave?.()
+      }
     },
-    onAfterLeave() {
-      this.scenesStore.previous.scene?.afterLeave?.()
+    created() {
+      this.scenesStore.setScenes(SCENES, UISceneNames.Start)
+
+      this.$wallet.on('state', (state) => {
+        this.walletStore.setState(state)
+      })
+
+      // Game Events
+      window.addEventListener('gameEvent', ClientEventsService.handleEvent);
+
+      // Global Events
+      window.addEventListener(
+        'resize',
+        debounce(() => {
+          this.$client.onWindowResize();
+          this.uiStore.viewport.setWindowWidth(window.innerWidth);
+        }, 200)
+      );
+
+      // INFO: client didnt send event when press escape
+      document.body.addEventListener('fullscreenchange', () => {
+        this.uiStore.fullscreen.active
+          ? this.uiStore.fullscreen.disable()
+          : this.uiStore.fullscreen.enable()
+      });
     }
-  },
-  created() {
-    this.scenesStore.setScenes(SCENES, UISceneNames.Start)
-
-    this.$wallet.on('state', (state) => {
-      this.walletStore.setState(state)
-    })
-
-    // Game Events
-    window.addEventListener('gameEvent', ClientEventsService.handleEvent);
-
-    // Global Events
-    window.addEventListener(
-      'resize',
-      debounce(() => {
-        this.$client.onWindowResize();
-        this.uiStore.viewport.setWindowWidth(window.innerWidth);
-      }, 200)
-    );
-
-    // INFO: client didnt send event when press escape
-    document.body.addEventListener('fullscreenchange', () => {
-      this.uiStore.fullscreen.active
-        ? this.uiStore.fullscreen.disable()
-        : this.uiStore.fullscreen.enable()
-    });
-  }
 };
 </script>
