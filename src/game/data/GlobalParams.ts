673fb0a2cd8474578ff681cfc04fd0c6fa187c20
Max <maxmonax@gmail.com>
2024-08-25 11:45:22 +0300

    v .511

--- src/game/data/GlobalParams.ts
+++ src/game/data/GlobalParams.ts
@@ -30,7 +30,7 @@ export type GalaxyData = {
  */
 export class GlobalParams {
 
-    static galaxyVersion = 'v0.3.510';
+    static galaxyVersion = 'v0.3.511';
     static starDefenderVersion = 'v0.2.0';
     static isDebugMode = false; // TODO: move to env parameters
     static isFPSShow = false; // TODO: move to env parameters
