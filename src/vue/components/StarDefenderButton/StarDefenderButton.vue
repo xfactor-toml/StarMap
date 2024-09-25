e3e26fcb7d6a9e2db10963b722c6ad01bccc437a
xfactor-toml <xfactor.toml@gmail.com>
2024-09-25 21:03:07 +0900

    fix: star-defender interpolation

--- src/vue/components/StarDefenderButton/StarDefenderButton.vue
+++ src/vue/components/StarDefenderButton/StarDefenderButton.vue
@@ -1,11 +1,11 @@
-<template>   
+<template>
     <div class="StarDefenderButton">
         <div class="StarDefenderButton__container" ref="container">
-                <div class="StarDefenderButton__connectLine">
+            <div class="StarDefenderButton__connectLine">
                 <img src="/gui/images/star-defender/connect-line.svg" />
                 <div class="StarDefenderButton__content">
-                    <div class="StarDefenderButton__bg" >
-                        <img src="/gui/images/star-defender/bg.svg"/>
+                    <div class="StarDefenderButton__bg">
+                        <img src="/gui/images/star-defender/bg.svg" />
                         <div class="StarDefenderButton__name" @click.stop="showStarTooltip">
                             {{ name }}
                         </div>
@@ -17,13 +17,12 @@
                         </div>
                     </div>
                 </div>
-                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script lang="ts">
-import { formatDuration } from '@/utils';
 import { PropType, watch } from 'vue';
 import { StarScreenPosition } from '@/models';
 import anime from 'animejs';
@@ -32,44 +31,56 @@ export default {
     name: 'StarDefenderButton',
     props: {
         name: {
-            type: String
+            type: String,
         },
         title: {
-            type:String
+            type: String,
         },
         position: {
-        type: Object as PropType<StarScreenPosition>
-        }
+            type: Object as PropType<StarScreenPosition>,
+        },
+    },
+    data() {
+        return {
+            isFirstLoad: true, 
+        };
     },
     mounted() {
-        this.updatePosition();
+        this.updatePosition(false); 
+        this.isFirstLoad = false; 
     },
     watch: {
         position: {
             handler() {
-                this.updatePosition();
+                this.updatePosition(true); 
             },
-            deep: true
-        }
+            deep: true,
+        },
     },
     methods: {
         handleClick() {
-            this.$emit('click')
+            this.$emit('click');
         },
         showStarTooltip() {
-            this.$emit('showStarTooltip')
+            this.$emit('showStarTooltip');
         },
-        updatePosition() {
-            anime({
-                targets: this.$refs.container,
-                width: `${this.position.x}px`,
-                height: `${this.position.y}px`,
-                easing: 'easeOutQuad',
-                duration: 500
-            });
-        }
-    }
-}
+        updatePosition(applyAnimation: boolean) {
+            if (this.isFirstLoad && !applyAnimation) {
+                this.$refs.container.style.width = `${this.position.x}px`;
+                this.$refs.container.style.height = `${this.position.y}px`;
+            } else {
+                anime({
+                    targets: this.$refs.container,
+                    width: `${this.position.x}px`,
+                    height: `${this.position.y}px`,
+                    easing: 'easeOutQuad',
+                    duration: 500,
+                });
+            }
+        },
+    },
+};
 </script>
 
 <style scoped src="./StarDefenderButton.css"></style>
+ 
